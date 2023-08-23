446913aedcff068617181ecf462d7c824c859e72
Prerana Kulkarni <81506047+Prerana0205@users.noreply.github.com>
2023-08-23 13:08:38 +0530

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Cart;
 class Product extends Model
 {
-    protected $table = 'Product';
     protected $fillable=['title','slug','summary','description','cat_id','child_cat_id','price','brand_id','discount','status','photo','quantity','unit','stock','is_featured','condition'];
 
     public function cat_info(){
